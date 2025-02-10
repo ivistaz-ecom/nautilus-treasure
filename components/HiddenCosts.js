e3d9585cbc6ca290e3b611a899630a7b7130249f
Bhavankumarg <gbhavanbkr@gmail.com>
2025-02-10 10:34:55 +0530

    hidden cost bg chianged

--- components/HiddenCosts.js
+++ components/HiddenCosts.js
@@ -17,7 +17,7 @@ export default function HiddenCosts() {
   };
 
   return (
-    <div className="bg-cover bg-center bg-[url('/home_banner.jpeg')] min-h-screen flex justify-center items-center">
+    <div className="bg-cover bg-[#00222F] min-h-screen flex justify-center items-center">
       {/* Content Section */}
       <div className="bg-[#00222F] w-full lg:max-w-[50%] text-white absolute text-center rounded-lg p-10 flex flex-col justify-between h-[100vh]">
         {/* Logo */}
